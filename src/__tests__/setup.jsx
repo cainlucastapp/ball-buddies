d6bdf400e3db8af2892ccc15d8a64ceb0afa805c
cainlucastapp <cainlucastapp@agentformula.com>
2025-12-15 10:47:27 -0800

    vite and package set up

---
+++ src/__tests__/setup.jsx
@@ -0,0 +1,15 @@
+import { afterEach, vi } from 'vitest'
+import { cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/vitest'
+  
+global.setFetchResponse = (val) => {
+    global.fetch = vi.fn(() => Promise.resolve({
+        json: () => Promise.resolve(val),
+        ok: true,
+        status: 200
+    }))
+}
+
+afterEach(() => {
+    cleanup();
+})
