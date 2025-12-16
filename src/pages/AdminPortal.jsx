7fcf24488051b8017a55cca6e59e39a700b9ee72
cainlucastapp <cainlucastapp@agentformula.com>
2025-12-15 16:20:32 -0800

    added new components files for AdminPortal

--- src/pages/AdminPortal.jsx
+++ src/pages/AdminPortal.jsx
@@ -6,8 +6,10 @@ const AdminPortal = () => {
         <div className="admin-container">
             <h1>Admin Portal</h1>
             <p>Manage your Ball Buddies inventory</p>
-            {/* ProductForm component will go here */}
-            {/* List of buddies for editing will go here */}
+            {/* page start with AdminForm */}
+            {/* Once logged in ProductList renders with stock toggle edit / delete buttons */}
+            {/* If create or edit is hit ProductForm component appears ProductList goes away and returns if cancle or submit*/}
+            
         </div>
     )
 }
