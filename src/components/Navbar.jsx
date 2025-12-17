293c0f341753ffca08dafddd5ae40cb7ee5fa5d5
cainlucastapp <cainlucastapp@agentformula.com>
2025-12-16 16:21:27 -0800

    Navbar styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,16 @@
 // src/components/Navbar.jsx
 
-// Dependancies
+// Dependencies
 import { NavLink } from "react-router-dom"
 import "../styles/components/Navbar.css"
 
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <div className="nav-brand">
-                <h1>Ball Buddies</h1>
-            </div>
             <ul className="nav-links">
-                <li><NavLink to="/" className={({ isActive }) => isActive ? "active" : ""}>Home</NavLink></li>
-                <li><NavLink to="/shop" className={({ isActive }) => isActive ? "active" : ""}>Shop</NavLink></li>
-                <li><NavLink to="/admin" className={({ isActive }) => isActive ? "active" : ""}>Admin Portal</NavLink></li>
+                <li><NavLink to="/">Home</NavLink></li>
+                <li><NavLink to="/shop">Shop</NavLink></li>
+                <li><NavLink to="/admin">Admin Portal</NavLink></li>
             </ul>
         </nav>
     )
