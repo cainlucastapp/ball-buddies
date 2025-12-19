7b16eb47bf55418f2bf8f21585539cc288d0a6b9
cainlucastapp <cainlucastapp@agentformula.com>
2025-12-19 11:06:24 -0800

    wrapped router with future flag to remove warnings

--- src/__tests__/components/Header.test.jsx
+++ src/__tests__/components/Header.test.jsx
@@ -6,6 +6,13 @@ import { render, screen } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom'
 import Header from '../../components/Header'
 
+// Wrapper with future flags
+const RouterWrapper = ({ children }) => (
+    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+        {children}
+    </BrowserRouter>
+);
+
 describe('Header', () => {
     
     describe('Base Tests', () => {
@@ -13,7 +20,7 @@ describe('Header', () => {
         // Test: Should render logo that links to home
         it('should render logo that links to home', () => {
             // Render component
-            render(<Header />, { wrapper: BrowserRouter });
+            render(<Header />, { wrapper: RouterWrapper });
             
             // Get logo link
             const logoLink = screen.getByRole('link', { name: /ball buddies/i });
@@ -26,7 +33,7 @@ describe('Header', () => {
         // Test: Should display tagline
         it('should display tagline', () => {
             // Render component
-            render(<Header />, { wrapper: BrowserRouter });
+            render(<Header />, { wrapper: RouterWrapper });
             
             // Check tagline is visible
             expect(screen.getByText('Where Every Ball Has Attitude!')).toBeInTheDocument();
