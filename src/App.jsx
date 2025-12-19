377bff00dc62354a42532b49e0564caac817bfa3
cainlucastapp <cainlucastapp@agentformula.com>
2025-12-19 10:09:13 -0800

    App render test

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,19 @@
-// App.jsx
+// src/__tests__/App.test.jsx
 
-//Dependencies
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Header from "./components/Header"
-import Home from "./pages/Home"
-import Shop from "./pages/Shop"
-import AdminPortal from "./pages/AdminPortal"
-import NotFound from "./pages/NotFound"
-import "./styles/App.css"
-import Footer from "./components/Footer"
+// Dependencies
+import { describe, it, expect } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import App from '../App'
 
-const App = () => {
-    return (
-        <>
-            <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-                <Header />
-                <div className="app-container">
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/shop" element={<Shop />} />
-                        <Route path="/admin" element={<AdminPortal />} />
-                        <Route path="*" element={<NotFound />} />
-                    </Routes>
-                </div>
-                <Footer />
-            </BrowserRouter>
-        </>
-    )
-}
-
-export default App
+describe('App', () => {
+    
+    // Test: Should render without crashing
+    it('should render without crashing', () => {
+        // Render app
+        render(<App />);
+        
+        // Renders header and footer
+        expect(screen.getByRole('banner')).toBeInTheDocument(); 
+        expect(screen.getByRole('contentinfo')).toBeInTheDocument();
+    });
+});
