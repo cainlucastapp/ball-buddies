4bafacfff4c0dcffad977fee03ed94da1aec845c
cainlucastapp <cainlucastapp@agentformula.com>
2025-12-19 12:32:06 -0800

    Added link to shop on 404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -11,6 +11,9 @@ const NotFound = () => {
             <h1>404</h1>
             <h2>Page Not Found</h2>
             <p>Oops! Looks like this buddy got lost.</p>
+            <Link to="/shop" className="notfound-link">
+                Click Here To Find Another Buddy
+            </Link>
         </div>
     )
 }
